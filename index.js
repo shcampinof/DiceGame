4aad742d206b92b8cc52047a3cc1a15b5e403fdf
shcampinof <shcampinof@unal.edu.co>
2022-02-25 05:38:55 -0500

    Adding animations and keydown eventLinstener

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-var randomNumber1=Math.floor(Math.random()*6+1);
-var randomNumber2=Math.floor(Math.random()*6+1);
+document.addEventListener("keydown", function(event) {
+  pressP(event.key)
+});
 
-if(randomNumber1>randomNumber2){
-document.querySelector("h1").textContent="🚩 Player 1 wins!";
-}
-else if(randomNumber1<randomNumber2){
-  document.querySelector("h1").textContent="Player 2 wins! 🚩";
-}
-else{
-  document.querySelector("h1").textContent="Draw!";
+function pressP(key) {
+
+  if (key === "p") {
+
+    var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+    var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+    if (randomNumber1 > randomNumber2) {
+
+      document.querySelector("h2").textContent = "🚩 Player 1 wins!";
+      restore();
+    } else if (randomNumber1 < randomNumber2) {
+      document.querySelector("h2").textContent = "Player 2 wins! 🚩";
+      restore();
+    } else {
+      document.querySelector("h2").textContent = "Draw!";
+      restore();
+    }
+
+    document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  }
 }
 
-document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
-document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+function restore() {
+
+  setTimeout(function() {
+    document.querySelector("h2").textContent = "Press [P] to Play!";
+  }, 2700);
+
+}
